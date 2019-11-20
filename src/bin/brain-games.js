dd6f5a799a6be84d456ddb14b9928ab63c780111
clickf5 <paninhome@gmail.com>
2019-11-20 09:17:41 +0300

    add index.js ang greeting function

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import greeting from '..';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
+
+// узнаем имя пользователя и приветсвуем его
+greeting();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+}
+
+export default greeting;
