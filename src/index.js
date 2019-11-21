c8505ab746d663f995d40165336803676182d2cd
clickf5 <paninhome@gmail.com>
2019-11-21 08:17:31 +0300

    Configurate eslint & travis-ci

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default greeting;
