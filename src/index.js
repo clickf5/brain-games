e2274b574fa4df554e707c3638e19e356a4a8a3c
clickf5 <paninhome@gmail.com>
2019-11-22 08:35:25 +0300

    fix complexity

--- src/index.js
+++ src/index.js
@@ -10,31 +10,31 @@ const isEven = (num) => (num % 2 === 0);
 
 const randNumGenerator = (min, max) => Math.round(Math.random() * (max - min) + min);
 
+const gameEvenStep = (stepCount) => {
+  if (stepCount > 3) {
+    return true;
+  }
+
+  const questionNum = randNumGenerator(1, 100);
+
+  const trueAnswer = isEven(questionNum) ? 'yes' : 'no';
+
+  const answer = readlineSync.question(`Question: ${questionNum}\nYour answer: `);
+
+  if (answer !== trueAnswer) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+    return false;
+  }
+
+  console.log('Correct!');
+  const newStepCount = stepCount + 1;
+  return gameEvenStep(newStepCount);
+};
+
 export const gameEven = () => {
   const userName = greeting();
 
-  const step = (stepCount) => {
-    if (stepCount > 3) {
-      return true;
-    }
-
-    const questionNum = randNumGenerator(1, 100);
-
-    const trueAnswer = isEven(questionNum) ? 'yes' : 'no';
-
-    const answer = readlineSync.question(`Question: ${questionNum}\nYour answer: `);
-
-    if (answer !== trueAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
-      return false;
-    }
-
-    console.log('Correct!');
-    const newStepCount = stepCount + 1;
-    return step(newStepCount);
-  };
-
-  if (step(1)) {
+  if (gameEvenStep(1)) {
     console.log(`Congratulations, ${userName}!`);
   } else {
     console.log(`Let's try again, ${userName}!`);
